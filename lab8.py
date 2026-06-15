34367726b4a18f90a9480c9741770809f66b24ed
VasundharaPremkumar <vasundhara6pk6@gmail.com>
2026-06-15 07:53:31 +0530

    Added ML Lab Programs

--- lab8.py
+++ lab8.py
@@ -4,17 +4,17 @@
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-
+from sklearn.metrics import adjusted_rand_score
 # Load Iris Dataset
 iris = load_iris()
 X = iris.data
-
+y=iris.target
 # Apply K-Means with 3 clusters
 kmeans = KMeans(n_clusters=3, random_state=42)
 
 # Predict cluster labels
 clusters = kmeans.fit_predict(X)
-
+print(adjusted_rand_score(y,clusters))
 # Visualize clusters
 plt.scatter(X[:,0], X[:,1], c=clusters)
 plt.xlabel("Sepal Length")
