4fd97c3d1c3b1abbc6e50c047960ae6c4298a54d
VasundharaPremkumar <vasundhara6pk6@gmail.com>
2026-07-28 08:37:23 +0530

    manual input for stepsize

--- lab1.py
+++ lab1.py
@@ -1,78 +1,27 @@
-from sklearn.datasets import load_iris
-import matplotlib.pyplot as plt
+# Hill Climbing Algorithm with Manual Step Size
 
-# Load dataset
-iris = load_iris()
+def f(x):
+    return -(x**2) + 5
 
-# Display column names
-print("Feature Names:")
-print(iris.feature_names)
+# User inputs
+x = float(input("Enter the starting value: "))
+step = float(input("Enter the step size: "))
 
-print("\nTarget Names:")
-print(iris.target_names)
+print("Step by step movement")
 
-# X-axis: Sepal Length
-x = iris.data[:, 0]
+while True:
+    print("Current x =", x, " f(x) =", f(x))
 
-# Y-axis: Target
-y = iris.target
+    left = x - step
+    right = x + step
 
-# Scatter Plot
-plt.scatter(x, y)
+    if f(left) > f(x):
+        x = left
+    elif f(right) > f(x):
+        x = right
+    else:
+        break
 
-plt.xlabel("Sepal Length")
-plt.ylabel("Target")
-plt.title("Sepal Length vs Target")
-
-plt.show()
-
-# Objective function that we want to maximize
-# f(x) = -x² + 5
-# Maximum value occurs at x = 0
-def objective(x):
-    return -x**2 + 5
-
-
-# Hill Climbing Algorithm
-def hill_climbing(start, step):
-
-    # Start from the initial state
-    current = start
-
-    # Repeat until no better neighbor is found
-    while True:
-
-        # Generate neighboring states
-        # One step to the left
-        left = current - step
-
-        # One step to the right
-        right = current + step
-
-        # If left neighbor has a better value,
-        # move to the left neighbor
-        if objective(left) > objective(current):
-            current = left
-
-        # Otherwise, if right neighbor has a better value,
-        # move to the right neighbor
-        elif objective(right) > objective(current):
-            current = right
-
-        # If neither neighbor is better,
-        # we have reached the peak
-        else:
-            break
-
-    # Return the best solution found
-    return current
-
-
-# Starting point of the search
-solution = hill_climbing(-0.1, 0.1)
-
-# Display the optimal solution
-print("Best Solution:", solution)
-
-# Display the maximum value of the objective function
-print("Maximum Value:", objective(solution))
+print("\nOptimal solution found!")
+print("Best x =", x)
+print("Maximum value f(x) =", f(x))
